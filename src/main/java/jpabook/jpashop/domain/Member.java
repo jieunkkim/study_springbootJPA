018ff889248f9c7a67016537598b58dfa5058534
jieunk.kim <wldmsidqwew@naver.com>
2021-08-31 09:05:26 +0900

    API 개발 기본 - 회원 등록, 수정, 조회 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +17,7 @@ public class Member {
     @Column(name = "member_id") // @Column 으로 name 따로 정의 안하면, field 명으로 column 명 따진다.
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
